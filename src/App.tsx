a558b9c5723959b62c733dd11a6303aff7c13f24
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2023-02-16 18:58:29 +0200

    Add i18n
    Commented example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
 import "./App.css";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import {useTranslation} from "react-i18next";
+import {ChangeLanguageButtons} from "./locales/ChangeLanguageButtons";
 
-const wait = (duration: number) =>
-  new Promise((resolve) => setTimeout(resolve, duration));
+// const wait = (duration: number) =>
+//   new Promise((resolve) => setTimeout(resolve, duration));
 
-const POSTS = [
-  { id: 1, title: "Post 1" },
-  { id: 2, title: "Post 2" },
-];
-
+// const POSTS = [
+//   { id: 1, title: "Post 1" },
+//   { id: 2, title: "Post 2" },
+// ];
 function App() {
-  const postsQuery = useQuery({
-    queryKey: ["posts"],
-    queryFn: () => wait(1000).then(() => [...POSTS]),
-  });
+  const {t} = useTranslation();
 
-  const newPostMutation = useMutation({
-    mutationFn: (title: string) =>
-      wait(1000).then(() => POSTS.push({ id: Number(new Date()), title })),
-  });
+  // const postsQuery = useQuery({
+  //   queryKey: ["posts"],
+  //   queryFn: () => wait(1).then(() => [...POSTS]),
+  // });
 
-  if (postsQuery.isLoading) return <h1>Loading...</h1>;
-  if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  // const newPostMutation = useMutation({
+  //   mutationFn: (title: string) =>
+  //     wait(1000).then(() => POSTS.push({ id: Number(new Date()), title })),
+  // });
+
+  // if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  // if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+
+
 
   return (
     <div>
-      {postsQuery.data.map((post) => (
-        <div key={post.id}>{post.title}</div>
-      ))}
-      <button onClick={() => newPostMutation.mutate("New Post")}>
-        Add New
-      </button>
+      <ChangeLanguageButtons />
+
+      {/*{postsQuery.data.map((post) => (*/}
+      {/*  <div key={post.id}>{post.title}</div>*/}
+      {/*))}*/}
+      {/*<button onClick={() => newPostMutation.mutate("New Post")}>*/}
+      {/*  Add New*/}
+      {/*</button>*/}
+
+
+<div>{t('greetings')}</div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, {Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import './i18n';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
