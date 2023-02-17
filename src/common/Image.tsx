4bf79466cc2c201fabb9d8dfc2a0bf0dc8fdb20d
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2023-02-17 17:37:10 +0200

    Add reusable input component

---
+++ src/common/Image.tsx
@@ -0,0 +1,49 @@
+import { useImageOnLoad } from '../utils'
+import { CSSProperties } from 'react'
+
+type ImageProps = {
+  src: {
+    thumbnail: string
+    fullSize: string
+  }
+  alt: string
+  loading?: 'lazy' | 'eager'
+  decoding?: 'async' | 'auto' | 'sync'
+}
+
+export const Image = ({ src, alt, loading = 'lazy', decoding = 'async' }: ImageProps) => {
+  const { handleImageOnLoad, css } = useImageOnLoad()
+
+  const style: CSSProperties = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: `100%`,
+    height: `100%`,
+  }
+
+  return (
+    <picture>
+      {/*<source type="image/webp" srcSet={''} />*/}
+      <img
+        src={src.thumbnail}
+        alt={alt}
+        style={{ ...style, ...(css.thumbnail as CSSProperties) }}
+        loading={loading}
+        decoding={decoding}
+        // srcSet={''}
+        sizes="(max-width: 800px) 100vw, 50vw"
+      />
+      <img
+        onLoad={handleImageOnLoad}
+        src={src.fullSize}
+        alt={alt}
+        style={{ ...style, ...(css.fullSize as CSSProperties) }}
+        loading={loading}
+        decoding={decoding}
+        // srcSet={''}
+        sizes="(max-width: 800px) 100vw, 50vw"
+      />
+    </picture>
+  )
+}
