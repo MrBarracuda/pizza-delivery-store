6fc0c6d1353dfe58a35d997c867cc45e8624cb76
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2023-02-17 20:36:05 +0200

    Remove unused exports

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,7 +1,3 @@
-export * from './useIsFirstRender'
 export * from './useMediaQuery'
-export * from './useEventCallback'
 export * from './useUpdateEffect'
-export * from './useIsomorphicLayoutEffect'
-export * from './useEventListener'
 export * from './useLocalStorage'
