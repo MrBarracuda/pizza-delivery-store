608b24058da3ba3db423234857a25493204626b8
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2023-02-18 23:26:05 +0200

    Add Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { RegistrationContainer } from './components'
 import { Footer } from './components/Footer'
 import { Navbar } from './components/Navbar'
 import { ChangeLanguageButtons } from './locales/ChangeLanguageButtons'
+import {Hero} from "./components/Hero";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero/>
       {/* <div className="mx-auto w-full max-w-xl"> */}
       {/*  <div className="my-12 flex justify-center"> */}
       {/*    <div className="w-full rounded-lg bg-white p-5 shadow-xl lg:w-11/12"> */}
