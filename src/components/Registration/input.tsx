2070a39c99f7fe887e1f4d706645aff218a4d30b
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2023-02-18 20:14:29 +0200

    Remove extra function wrapper

--- src/components/Registration/input.tsx
+++ src/components/Registration/input.tsx
@@ -4,22 +4,22 @@ import { ErrorMessage } from './ErrorMessage'
 
 import type { InputProps } from './types'
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(function Input({ label, type = 'text', ...props }, ref) {
-  return (
-    <div className="mb-4 md:mr-2 md:mb-0">
-      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <label className="mb-2 block text-sm font-bold text-gray-700">{label}</label>
-      <input
-        type={type}
-        ref={ref}
-        className={
-          props.className
-            ? props.className
-            : 'focus:shadow-outline w-full appearance-none rounded border px-3 py-2 text-sm leading-tight text-gray-700 focus:outline-none'
-        }
-        {...props}
-      />
-      <ErrorMessage name={props.name} />
-    </div>
-  )
-})
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ label, type = 'text', name, ...props }, ref) => (
+  <div className="mb-4 md:mr-2 md:mb-0">
+    <label htmlFor={name} className="mb-2 block text-sm font-bold text-gray-700">
+      {label}
+    </label>
+    <input
+      type={type}
+      ref={ref}
+      name={name}
+      className={
+        props.className
+          ? props.className
+          : 'focus:shadow-outline w-full appearance-none rounded border px-3 py-2 text-sm leading-tight text-gray-700 focus:outline-none'
+      }
+      {...props}
+    />
+    <ErrorMessage name={name} />
+  </div>
+))
