03c2a67405137764e50dc0a29de2bb712a14023c
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2023-02-18 23:09:59 +0200

    Remove default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import './App.css'
 
 import { RegistrationContainer } from './components'
+import { Footer } from './components/Footer'
+import { Navbar } from './components/Navbar'
 import { ChangeLanguageButtons } from './locales/ChangeLanguageButtons'
 
 function App() {
   return (
-    <div className="mx-auto w-full max-w-xl">
-      <div className="my-12 flex justify-center">
-        <div className="w-full rounded-lg bg-white p-5 shadow-xl lg:w-11/12">
-          <RegistrationContainer />
-          <ChangeLanguageButtons />
-        </div>
-      </div>
+    <div>
+      <Navbar />
+      {/* <div className="mx-auto w-full max-w-xl"> */}
+      {/*  <div className="my-12 flex justify-center"> */}
+      {/*    <div className="w-full rounded-lg bg-white p-5 shadow-xl lg:w-11/12"> */}
+      <RegistrationContainer />
+      <ChangeLanguageButtons />
+      {/*    </div> */}
+      {/*  </div> */}
+      {/* </div> */}
+      <Footer />
     </div>
   )
 }
