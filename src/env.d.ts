a4e49c35a49f9c3801f979860c2cd279d48cd631
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2023-02-25 16:27:41 +0200

    Re-Structure project

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_API_KEY: string
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_APP_AUTH_TOKEN: string
+  readonly VITE_APP_API_URL: string
+  readonly VITE_NODE_ENV: string
 }
 
 interface ImportMeta {
